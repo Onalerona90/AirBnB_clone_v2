f7d53e66c4d5fa19027f6af6c41095a5696e0e3a
Tebogo Ndaba <dennistebogo@gmail.com>
2024-05-25 04:40:17 +0200

    Number template

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -74,7 +74,7 @@ def number_template(n):
     Displays a HTML page only if n is an integer
     H1 tag: “Number: n” inside the tag BODY
     """
-    return render_template('5-number.html', number=n)
+    return render_template('5-number.html', n=n)
 
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
