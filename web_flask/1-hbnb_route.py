92963a22b7c063c5a05bd8b5d7a9f19322b2f8c8
Tebogo Ndaba <dennistebogo@gmail.com>
2024-05-25 03:54:17 +0200

    HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,12 +13,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
     Displays 'Hello HBNB!' at the root URL
     """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
