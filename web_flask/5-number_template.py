d31c3414fec64558ba2aa5ae0e4aa5b3932b43f7
Tebogo Ndaba <dennistebogo@gmail.com>
2024-05-25 04:14:27 +0200

    Number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -72,7 +72,7 @@ def number_template(n):
     Displays an HTML page only if n is an integer:
     H1 tag: “Number: n” inside the tag BODY
     """
-    return render_template('number.html', number=n)
+    return render_template('5-number.html', number=n)
 
 
 if __name__ == '__main__':
